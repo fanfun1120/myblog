95ae29f5c92959a2c0adeae6cd21e4db45c69c99
fanhao <fanfun1120@qq.com>
2022-08-25 11:00:12 +0800

    修改基本信息

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# myblog
-个人博客
+个人博客项目
+
+- 如何启动？
+  1. 创建数据库名为：blog的数据库，然后项目目录中有个sql文件夹，进入后找到blog.sql并运行在blog数据库
+  2. 修改application.yml中数据库连接信息
+  3. 启动redis
+  4. 运行FanBlogApplication中main方法。
+  5. 运行成功后，浏览器打开地址localhost:7777
+  6. 后台管理模块的路由地址是localhost:7777/admin
+- 注意点
+  1. **如果想清空数据库内容后使用，记得一定要在blog数据库中的t_user表中插入一条数据，填写全。t_user表中avatar字段要填写的内容例如：/images/myavatar.jpg。在myblog\src\main\resources\static\images这个目录中有张图片名字是myavatar.jpg**
+
--- src/test/java/com/fanfun/fan_blog/FanBlogApplicationTests.java
+++ src/test/java/com/fanfun/fan_blog/FanBlogApplicationTests.java
@@ -1,6 +1,5 @@
 package com.fanfun.fan_blog;
 
-import com.fanfun.test.SysProductOutlay;
 import com.fanfun.util.RedisCache;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,34 +26,6 @@ class FanBlogApplicationTests {
         redisCache.setCacheObject("testredis", "123");
         String testredis = (String) redisCache.getCacheObject("testredis");
         System.out.println(testredis);
-    }
-
-    @Test
-    public void testList() {
-        //新建list 添加顺序与分区号一致
-        List<SysProductOutlay> codeList = new ArrayList<>();
-        List<SysProductOutlay> sysProductOutlayList = new ArrayList<>();
-
-//        List<SysProductOutlay> list = new ArrayList<>();
-        //遍历分区集合，确保添加价格时 与分区号出现的顺序一致
-        for (SysProductOutlay productOutlay : codeList) {
-            for (SysProductOutlay product : sysProductOutlayList) {
-                //当分区号相同时添加价格到集合中
-                if (productOutlay.getAreaCode().equals(product.getAreaCode())) {
-                    list.add(product);
-                }
-            }
-        }
-
-        Map<Integer, SysProductOutlay> collect = codeList.stream().collect(Collectors.toMap(productOutlay -> productOutlay.getAreaCode(),
-                productOutlay -> productOutlay
-        ));
-        List<SysProductOutlay> list = sysProductOutlayList.stream().map(product -> {
-                    return codeList.get(product.getAreaCode());
-                }
-        ).collect(Collectors.toList());
-
-
     }
 
 }
